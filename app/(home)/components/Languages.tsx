ad6b0f0fe227713258f403ce586368399d7d3c75
Soumadeep Singha <79574998+SSingha2@users.noreply.github.com>
2024-09-04 21:12:23 +0530

    Update Languages.tsx
    Passed JLPT N2

--- app/(home)/components/Languages.tsx
+++ app/(home)/components/Languages.tsx
@@ -9,7 +9,7 @@ export default function Languages() {
       <Title text="Languages Known" className='flex flex-col items-center justify-center rotate-6 pt-10'/>
       <div className='grid grid-cols-1 sm:grid-cols-2 gap-5 m-20'>
         <MovingBorderBtn className='p-10 text-xl font-bold italic'>English</MovingBorderBtn>
-        <MovingBorderBtn className='p-10 text-xl font-bold italic'><Link href={"https://drive.google.com/file/d/12BF52dy2Kp4SnfUPdLxV6A51HOwjGATg/view?usp=sharing"} target="_blank">Japanese (JLPT N3)</Link></MovingBorderBtn>
+        <MovingBorderBtn className='p-10 text-xl font-bold italic'><Link href={"https://drive.google.com/file/d/12BF52dy2Kp4SnfUPdLxV6A51HOwjGATg/view?usp=sharing"} target="_blank">Japanese (JLPT N2)</Link></MovingBorderBtn>
       </div>
     </div>
   )
